7159fce80f3aac24d327c9f990e72aa44fc9d126
abhirupkgp (Mr. ) <abhirupkgp@gmail.com>
2013-06-28 03:50:43 +0000

    bfgs working code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rnso@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- Hanso_files/bfgs.R
+++ Hanso_files/bfgs.R
@@ -1,48 +1,49 @@
-bfgs <- function(fn,gr,nvar,maxit = 1000, normtol = 1e-6, 
+bfgs <- function(fn,gr,nvar,nstart=10,x0 = matrix(rnorm(nvar*nstart),nvar,nstart),maxit = 1000, normtol = 1e-6, 
 		      fvalquit = -Inf, xnormquit = Inf, nvec = 0, prtlevel = 1,
 		      strongwolfe = 0, wolfe1 = 1e-4, wolfe2 = 0.5, quitLSfail = 1,
-		      ngrad = 2, evaldist = 1e-4, H0 = diag(nvar), scale = 1,nstart=10,
-		      x0 = matrix(rnorm(nvar*nstart),nvar,nstart){
-  x <- list()
+		      ngrad = 2, evaldist = 1e-4, H0 = diag(nvar), scale = 1)
+		      {
+  x <- matrix(NA,nvar,nstart)
   f <- c()
   d <- list()
   iter <- c()
-  #HH <- list()
-  H <- list()
+  
+  HH <-list()
   info <- c()
   X <- list()
   G <- list()
   w <- list()
-  fevalrec <- c()
+  fevalrec <- list()
   xrec <- list()
   Hrec <- list()
   
   for(run in 1:nstart){
-    tmp <- bfgs1run(fn,gr,options$x0[,run],options)
+    tmp <- bfgs1run(fn,gr,nvar=2,x0[,run])
     #print(tmp)
-    x[[run]] <- tmp$x
+    x[,run] <- tmp$x
     f[run] <- tmp$f
     d[[run]] <- tmp$d
-    HH <- as.matrix(tmp$H)
+    H <- as.matrix(tmp$H)
     iter[run] <- tmp$iter
     info[run] <- tmp$info
     X[[run]] <- tmp$X
     G[[run]] <- tmp$G
     w[[run]] <- tmp$w
-    fevalrec[run] <- tmp$fevalrec
+    fevalrec[[run]] <- tmp$fevalrec
     xrec[[run]] <- tmp$xrec
     Hrec[[run]] <- tmp$Hrec
   }
-  H[[run]] <- (HH+t(HH))/2
-  #cputime break condition
-  if(nstart == 1){
-    H <- H[1]
-    fevalrec <- fevalrec[1]
-    xrec <- xrec[1]
-    Hrec <- Hrec[1]
-    X <- X[1]
-    G <- G[1]
-    w <- w[1]
-  }
-  return(list(x,f,d,H,iter,X,G,fevalrec,xrec,Hrec))
+  HH[[run]] <- (H+t(H))/2
+  #no need for cpu break
+  #no need for special formatting for nstart==1
+#   if(nstart == 1){
+#     H <- H[1]
+#     fevalrec <- fevalrec[1]
+#     xrec <- xrec[1]
+#     Hrec <- Hrec[1]
+#     X <- X[1]
+#     G <- G[1]
+#     w <- w[1]
+#   }
+  return(list(x=x,f=f,d=d,H=HH,iter=iter,X=X,G=G,fevalrec=fevalrec,xrec=xrec,Hrec=Hrec))
 }
