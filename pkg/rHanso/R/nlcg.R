2cc132d0b1921d9107fd42cec26bc684f5adeb47
abhirupkgp (Mr. ) <abhirupkgp@gmail.com>
2013-08-14 17:33:08 +0000

    formatting changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rnso@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rHanso/R/nlcg.R
+++ pkg/rHanso/R/nlcg.R
@@ -1,39 +1,39 @@
 nlcg <- function(fn, gr = NULL, nvar = 0, nstart = 10, x0 = NULL, upper = 1, lower = 0, H0 = NULL, maxit = 1000, 
-    fvalquit = -Inf, prtlevel = 0, version = "C", normtol = 1e-06, xnormquit = Inf, evaldist = 1e-04, ngrad = 0, 
-    scale = 1, wolfe1 = 1e-04, wolfe2 = 0.5, quitLSfail = TRUE, strongwolfe = 1) {
-    if (!is.null(x0)) {
-        
-        if (class(x0) == "numeric") {
-            x0 <- t(x0)
-            nstart <- 1
-            nvar = length(x0)
-        } else if (class(x0) == "matrix") {
-            nvar <- nrow(x0)
-            nstart <- ncol(x0)
-        } else stop("unknown initial value matrix, please enter a numeric vector or matrix")
-    } else {
-        nstart <- 10
-        M <- matrix(runif(nvar * nstart), nrow = nvar, ncol = nstart)
-        x0 <- (upper - lower) * M + lower
-    }
+  fvalquit = -Inf, prtlevel = 0, version = "C", normtol = 1e-06, xnormquit = Inf, evaldist = 1e-04, ngrad = 0, 
+  scale = 1, wolfe1 = 1e-04, wolfe2 = 0.5, quitLSfail = TRUE, strongwolfe = 1) {
+  if (!is.null(x0)) {
     
-    
-    x <- matrix(NA, nvar, nstart)
-    f <- c()
-    g <- matrix(NA, nvar, nstart)
-    frec <- list()
-    alpharec <- list()
-    message <- list()
-    for (run in 1:nstart) {
-        tmp <- nlcg1run(fn, gr, x0[, run], H0, maxit, fvalquit, strongwolfe, version, prtlevel, normtol, 
-            xnormquit, evaldist, ngrad, scale, wolfe1, wolfe2, quitLSfail)
-        # print(tmp)
-        x[, run] <- tmp$x
-        f[run] <- tmp$f
-        g[, run] <- tmp$g
-        frec[[run]] <- tmp$frec
-        alpharec[[run]] <- tmp$alpharec
-        message[[run]] <- tmp$message
-    }
-    list(x = x, f = f, g = g, frec = frec, alpharec = alpharec, message = message)
+    if (class(x0) == "numeric") {
+      x0 <- t(x0)
+      nstart <- 1
+      nvar = length(x0)
+    } else if (class(x0) == "matrix") {
+      nvar <- nrow(x0)
+      nstart <- ncol(x0)
+    } else stop("unknown initial value matrix, please enter a numeric vector or matrix")
+  } else {
+    nstart <- 10
+    M <- matrix(runif(nvar * nstart), nrow = nvar, ncol = nstart)
+    x0 <- (upper - lower) * M + lower
+  }
+  
+  
+  x <- matrix(NA, nvar, nstart)
+  f <- c()
+  g <- matrix(NA, nvar, nstart)
+  frec <- list()
+  alpharec <- list()
+  message <- list()
+  for (run in 1:nstart) {
+    tmp <- nlcg1run(fn, gr, x0[, run], H0, maxit, fvalquit, strongwolfe, version, prtlevel, normtol, 
+      xnormquit, evaldist, ngrad, scale, wolfe1, wolfe2, quitLSfail)
+    # print(tmp)
+    x[, run] <- tmp$x
+    f[run] <- tmp$f
+    g[, run] <- tmp$g
+    frec[[run]] <- tmp$frec
+    alpharec[[run]] <- tmp$alpharec
+    message[[run]] <- tmp$message
+  }
+  list(x = x, f = f, g = g, frec = frec, alpharec = alpharec, message = message)
 } 
--- pkg/rHanso/R/shor.R
+++ pkg/rHanso/R/shor.R
@@ -1,50 +1,50 @@
 shor <- function(fn, gr = NULL, nvar = 0, nstart = 10, x0 = NULL, upper = 1, lower = 0, maxit = 1000, fvalquit = -Inf, 
-    beta = 0.5, normtol = 1e-06, xnormquit = Inf, evaldist = 1e-04, ngrad = 0, rescale = 0, strongwolfe = 0, 
-    useprevstep = 0, wolfe1 = 1e-04, wolfe2 = 0.5, quitLSfail = TRUE, prtlevel = 1) {
+  beta = 0.5, normtol = 1e-06, xnormquit = Inf, evaldist = 1e-04, ngrad = 0, rescale = 0, strongwolfe = 0, 
+  useprevstep = 0, wolfe1 = 1e-04, wolfe2 = 0.5, quitLSfail = TRUE, prtlevel = 1) {
+  
+  if (!is.null(x0)) {
     
-    if (!is.null(x0)) {
-        
-        if (class(x0) == "numeric") {
-            x0 <- matrix(x0)
-            nstart <- 1
-            nvar = length(x0)
-        } else if (class(x0) == "matrix") {
-            nvar <- nrow(x0)
-            nstart <- ncol(x0)
-        } else stop("unknown initial value matrix, please enter a numeric vector or matrix")
-    } else {
-        nstart <- 10
-        M <- matrix(runif(nvar * nstart), nrow = nvar, ncol = nstart)
-        x0 <- (upper - lower) * M + lower
-    }
+    if (class(x0) == "numeric") {
+      x0 <- matrix(x0)
+      nstart <- 1
+      nvar = length(x0)
+    } else if (class(x0) == "matrix") {
+      nvar <- nrow(x0)
+      nstart <- ncol(x0)
+    } else stop("unknown initial value matrix, please enter a numeric vector or matrix")
+  } else {
+    nstart <- 10
+    M <- matrix(runif(nvar * nstart), nrow = nvar, ncol = nstart)
+    x0 <- (upper - lower) * M + lower
+  }
+  
+  
+  x <- matrix(NA, nvar, nstart)
+  f <- c()
+  g <- matrix(NA, nvar, nstart)
+  B <- list()
+  frec <- list()
+  fevalrec <- list()
+  betarec <- list()
+  xrec <- list()
+  svrec <- list()
+  
+  for (run in 1:nstart) {
+    res <- shor1run(fn, gr, x0[, run], maxit, fvalquit, beta, normtol, xnormquit, evaldist, ngrad, rescale, 
+      strongwolfe, useprevstep, wolfe1, wolfe2, quitLSfail, prtlevel)
+    x[, run] <- res$x
+    f[run] <- res$f
+    g[, run] <- res$g
+    B[[run]] <- res$B
+    frec[[run]] <- res$frec
+    fevalrec[[run]] <- res$fevalrecall
+    betarec[[run]] <- res$betarec
+    xrec[[run]] <- res$xrec
+    svrec[[run]] <- res$svrec
     
-    
-    x <- matrix(NA, nvar, nstart)
-    f <- c()
-    g <- matrix(NA, nvar, nstart)
-    B <- list()
-    frec <- list()
-    fevalrec <- list()
-    betarec <- list()
-    xrec <- list()
-    svrec <- list()
-    
-    for (run in 1:nstart) {
-        res <- shor1run(fn, gr, x0[, run], maxit, fvalquit, beta, normtol, xnormquit, evaldist, ngrad, rescale, 
-            strongwolfe, useprevstep, wolfe1, wolfe2, quitLSfail, prtlevel)
-        x[, run] <- res$x
-        f[run] <- res$f
-        g[, run] <- res$g
-        B[[run]] <- res$B
-        frec[[run]] <- res$frec
-        fevalrec[[run]] <- res$fevalrecall
-        betarec[[run]] <- res$betarec
-        xrec[[run]] <- res$xrec
-        svrec[[run]] <- res$svrec
-        
-    }
-    
-    return(list(x = x, f = f, g = g, B = B, frec = frec, fevalrec = fevalrec, betarec = betarec, xrec = xrec, 
-        svrec = svrec))
-    
+  }
+  
+  return(list(x = x, f = f, g = g, B = B, frec = frec, fevalrec = fevalrec, betarec = betarec, xrec = xrec, 
+    svrec = svrec))
+  
 } 
