89cc6943411790b326277027df7d12a290b2dac8
abhirupkgp (Mr. ) <abhirupkgp@gmail.com>
2013-08-26 00:41:38 +0000

    remove write_best_to_x
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rnso@89 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rImfil/R/write_best_to_x.R
+++
@@ -1,6 +0,0 @@
-write_best_to_x <- function(iteration_data) {
-  x <- iteration_data$xb
-  funs <- iteration_data$funsb
-  fval <- iteration_data$fobjb
-  return(list(x = x, funs = funs, fval = fval))
-}
